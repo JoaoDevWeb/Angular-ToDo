4183c4d7b58b4669b4eacd029244a60a1c248634
João Vitor <jv17fc@gmail.com>
2020-04-25 14:34:57 -0300

    Validando checkBox e armazenando dados com o localStorage

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -7,16 +7,20 @@ import { Task } from './task';
 export class TaskService {
 
   tasks: Task[] = [
-    { id: 1, description: 'Tarefa 1', completed: false},
-    { id: 2, description: 'Tarefa 2', completed: true},
-    { id: 3, description: 'Tarefa 3', completed: true},
-    { id: 4, description: 'Tarefa 4', completed: false},
-    { id: 5, description: 'Tarefa 5', completed: true},
+    // { id: 1, description: 'Tarefa 1', completed: false},
+    // { id: 2, description: 'Tarefa 2', completed: true},
+    // { id: 3, description: 'Tarefa 3', completed: true},
+    // { id: 4, description: 'Tarefa 4', completed: false},
+    // { id: 5, description: 'Tarefa 5', completed: true},
   ];
 
   constructor() { }
 
   getAll() {
+    const list = window.localStorage.getItem('lista-tarefas');
+    if (list) {
+      this.tasks = JSON.parse(list);
+    }
     return this.tasks;
   }
 
@@ -31,16 +35,24 @@ export class TaskService {
       taskArr.description = task.description;
       task.completed = task.completed;
     } else {
-      const lastId = this.tasks[this.tasks.length-1].id;
+      // const lastId = this.tasks[this.tasks.length-1].id;
+      let lastId = 0;
+      if (this.tasks.length > 0) {
+        lastId = this.tasks[this.tasks.length -1].id;
+      }
+
       task.id = lastId + 1;
       task.completed = false;
       this.tasks.push(task);
     }
+
+    window.localStorage.setItem('lista-tarefas', JSON.stringify(this.tasks));
   }
 
   delete(id: number) {
     const taskIndex = this.tasks.findIndex((value) => value.id == id);
     this.tasks.splice(taskIndex, 1);
+    window.localStorage.setItem('lista-tarefas', JSON.stringify(this.tasks));
   }
 
 }
--- src/app/tasks/task-list-item/task-list-item.component.ts
+++ src/app/tasks/task-list-item/task-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../shared/task';
+import { TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'app-task-list-item',
@@ -10,9 +11,17 @@ export class TaskListItemComponent implements OnInit {
   @Input()
   task: Task;
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+  }
+
+  remove(task: Task) {
+    this.taskService.delete(task.id);
+  }
+
+  onCompletedCheckChange(task: Task) {
+    this.taskService.save(task);
   }
 
 }
