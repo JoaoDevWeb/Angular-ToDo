d153aa8a09721b2a6f04ff3a7d5e821270d91dcf
João Vitor <jv17fc@gmail.com>
2020-04-24 21:30:35 -0300

    Criando componentes, service e classe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskListItemComponent } from './task/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskListItemComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule
